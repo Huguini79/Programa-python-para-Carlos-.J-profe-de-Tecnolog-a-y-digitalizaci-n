4ee37557ac6a7eca26e524d7c6cb21361f11051f
Huguini79 <166934655+Huguini79@users.noreply.github.com>
2024-09-20 20:37:15 +0100

    Add files via upload

---
+++ programita.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import messagebox
+import random
+
+root = tk.Tk()
+root.configure(bg='yellow')
+root.geometry('660x550')
+root.title('Programa por Hugo González Ruiz de 1 ESO D')
+personas = []
+def agregar():
+    texto2 = texto.get('1.0', 'end-1c')
+    personas.append(texto2);
+def ale():
+    per_ale = random.choice(personas)
+    messagebox.showinfo('Persona aleatoria', f'Persona aleatoria: {per_ale}')
+label = tk.Label(root, text='Bienvenido a mi programa en python por mí(Hugo)', height='2', width='60')
+texto = tk.Text(root, height='2', width='25')
+botónagreg = tk.Button(root, text='Agregar persona', height='2', width='40', command=agregar)
+botónale = tk.Button(root, text='Mostrar persona aleatoria', height='2', width='50', command=ale)
+label.grid(row=0, column=0, padx=10, pady=10)
+texto.grid(row=1, column=0, padx=10, pady=10)
+botónagreg.grid(row=2, column=0, padx=10, pady=10)
+botónale.grid(row=3, column=0, padx=10, pady=10)
+root.mainloop()
